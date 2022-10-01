d3ee9f1ecc00149c55e29cb6700f3932a3ea81e1
stefangohlke <gohlke.stefan@yahoo.de>
2022-10-01 22:26:22 +0200

    Add graphqlserver v3 example

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # koa-example
-Example NodeJS app using koa
+Example NodeJS app using koa with GraphQLServer
+
+## How to run
+To install the necessary dependencies run **npm install**. To run the server execute **npm start**. It will run a Fastify server using GraphQLServer on localhost:7070/graphql with the user example schema and resolvers from the `@dreamit/graphql-server` project.
---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import { 
+    GraphQLServer, 
+    JsonLogger 
+} from '@dreamit/graphql-server'
+import { 
+    userSchema, 
+    userSchemaResolvers 
+} from './ExampleSchemas'
+import Koa from 'koa'
+import bodyParser from 'koa-bodyparser'
+
+const graphqlServer = new GraphQLServer(
+    {
+        schema: userSchema,
+        rootValue: userSchemaResolvers,
+        logger:  new JsonLogger('koaServer', 'user-service'),
+    }
+)
+
+const server = new Koa()
+server.use(bodyParser())
+
+server.use(async context => {
+    await graphqlServer.handleRequest(context.request , { 
+        statusCode: context.response.status,
+        setHeader: function(name, value) {
+            context.response.set(name, value as string)
+            return this 
+        },
+        removeHeader: function(name) {
+            context.response.remove(name)
+        },
+        end: function(chunk) {
+            context.body = chunk
+            return this
+        },
+    })
+})
+  
+server.listen(7070)
